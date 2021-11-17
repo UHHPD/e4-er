e0463a4f5e77531cb1c816032dafca1049bd44e5
Hartmut Stadie <hartmut.stadie@desy.de>
2021-11-17 16:50:39 +0100

    Hoffentlich alles richtig...

--- Data.cc
+++ Data.cc
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <cassert>
 #include <stdexcept>
+#include <cmath>
 
 using namespace std;
 
@@ -31,6 +32,13 @@ Data::Data(const std::string& filename) {
     file >> entries;
     m_data.push_back(entries);
   }
+  // read in errors from file: m_siz bin errors
+  for (int i = 0; i < size; ++i) {
+    double errors;
+    file >> errors;
+    m_errors.push_back(errors);
+  }
+ 
 
   // done! close the file
   file.close();
@@ -39,3 +47,31 @@ Data::Data(const std::string& filename) {
 };
 
 void Data::assertSizes() { assert(m_data.size() + 1 == m_bins.size()); }
+
+int Data::checkCompatibility(const Data& in, int n) {
+  int j = 0;
+  for (int i = 0; i < size(); ++i) {
+    double diff = abs(measurement(i)-in.measurement(i));
+    double err = sqrt(pow((error(i)),2)+pow((in.error(i)),2));
+    if (n*err < diff) {
+      j += 1;
+    }
+  }
+  return j;
+};
+
+double background(double x) {
+  double f_x = 0.005 - 0.00001 * x + 0.08 * exp(-0.015 * x);
+  return f_x;
+}
+
+double Data::chisquared_ndf(int parameters) {
+  double chisum = 0;
+  for (int i = 0; i < size(); i++) {
+    double delta = (measurement(i) - background(binCenter(i)));
+    double chi = pow((delta/error(i)),2);
+    chisum += chi;
+  }
+  double chi2_ndf = chisum / (size() - parameters);
+  return chi2_ndf;
+}
--- run.cc
+++ run.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <functional>
 #include <string>
+#include <cmath>
 
 #include "Data.hh"
 
@@ -60,6 +61,22 @@ void runTests() {
     std::cout << (test() ? " ok" : " FAILED!") << std::endl;
 }
 
+/*
+
+Data calcAverage(const Data& in1, const Data& in2) {
+  Data averages;
+  for (int i = 0; i < in1.size(); ++i) {
+    double w1 = 1/pow(in1.error(i),2);
+    double w2 = 1/pow(in2.error(i),2);
+    double y = (w1 * in1.measurement(i) + w2 * in2.measurement(i))/(w1+w2);
+    averages.push_back(y);
+    std::cout << "Average" << i << ": " << y << std::endl;
+  }
+  return averages;
+}
+
+*/
+
 int main() {
   using namespace std;
 
@@ -68,12 +85,77 @@ int main() {
   cout << "******************************************************" << endl;
   // create an object which holds data of experiment A
   Data datA("exp_A");
+  // create an object which holds data of experiment B
+  Data datB("exp_B");
+  // create an object which holds data of experiment C
+  Data datC("exp_C");
+  // create an object which holds data of experiment D
+  Data datD("exp_D");
 
   // here is the data from experiment A
   cout << "bin 27: from " << datA.binLow(27) << " to " << datA.binHigh(27)
        << endl;
   cout << "measurement of experiment A in bin 27: " << datA.measurement(27)
        << endl;
+  cout << "measurement of experiment B in bin 27: " << datB.measurement(27)
+       << endl;
+  cout << "measurement of experiment C in bin 27: " << datC.measurement(27)
+       << endl;
+  cout << "measurement of experiment D in bin 27: " << datD.measurement(27)
+       << endl;  
+       
+       
+  // Check if A and B are compatible
+  cout << "|A-B| = " << sqrt(pow((datA.measurement(27)-datB.measurement(27)),2))
+       << endl;
+  cout << "Error in A " << datA.error(27)
+       << endl;
+  cout << "Error in B " << datB.error(27)
+       << endl;
+  cout << "Resulting Error: " << sqrt(pow((datA.error(27)),2)+pow((datB.error(27)),2))
+       << endl;
+       
+  //cout << "first test of compatibility function: " << //datA.checkCompatibility(datB,1)
+  //     << endl;
+       
+  //First test of calcAverage
+  Data sum12 = datA + datB;
+  cout << "sum12 (sum of datA and datB) in bin 27: " << sum12.measurement(27)
+     << endl;
+       
+  //Output deviations between datasets
+  cout << "Checking compatibility of datasets for 2 sigma: " << endl << endl;
+  cout << "A and B: " << datA.checkCompatibility(datB,2) << endl; 
+  cout << "A and C: " << datA.checkCompatibility(datC,2) << endl; 
+  cout << "A and D: " << datA.checkCompatibility(datD,2) << endl; 
+  cout << "B and C: " << datB.checkCompatibility(datC,2) << endl; 
+  cout << "B and D: " << datB.checkCompatibility(datD,2) << endl; 
+  cout << "C and D: " << datC.checkCompatibility(datD,2) << endl << endl; 
+  
+  cout << "Checking compatibility of datasets for 3 sigma: " << endl << endl;
+  cout << "A and B: " << datA.checkCompatibility(datB,3) << endl; 
+  cout << "A and C: " << datA.checkCompatibility(datC,3) << endl; 
+  cout << "A and D: " << datA.checkCompatibility(datD,3) << endl; 
+  cout << "B and C: " << datB.checkCompatibility(datC,3) << endl; 
+  cout << "B and D: " << datB.checkCompatibility(datD,3) << endl; 
+  cout << "C and D: " << datC.checkCompatibility(datD,3) << endl << endl; 
+       
+  //Output compatibility with background
+  cout << "Calculating Chisquared for datasets with background: " << endl << endl;
+  cout << "Chisquared/ndf for A: " << datA.chisquared_ndf(4) << endl;     
+  cout << "Chisquared/ndf for B: " << datB.chisquared_ndf(4) << endl; 
+  cout << "Chisquared/ndf for C: " << datC.chisquared_ndf(4) << endl; 
+  cout << "Chisquared/ndf for D: " << datD.chisquared_ndf(4) << endl << endl;   
+  
+  //Combining the datasets
+  Data sumAB = datA + datB;
+  Data sumCD = datC + datD;
+  Data sumall = sumAB + sumCD;
+  cout << "Test of sumall: " << sumall.error(27) << " and " << sumall.binLow(27) << endl;
+  //Data sumall = datA + datB + datC + datD; 
+  
+  cout << "Chisquared for combined datasets with background: " << 
+  sumall.chisquared_ndf(4) << endl << endl;
 
   return 0;
 }
